7751d28108adf0ad78fba708ce1cbc48e3cdcdc7
ahnilhyuk <ilhyuk0504@gmail.com>
2025-04-16 22:53:38 +0900

    third

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
-let swiperContainer = document.querySelector('.swiper-container')
-let swiper = new Swiper('.swiper-container', {
+let swiperContainer = document.querySelector('.content_box')
+let swiper = new Swiper('.content_box', {
     // 한 번에 보여줄 슬라이드 수
     slidesPerView: 1,
     // 슬라이드 사이의 간격
@@ -7,13 +7,32 @@ let swiper = new Swiper('.swiper-container', {
     // 한 번에 넘길 슬라이드 수
     slidesPerGroup: 1, 
     loop: true,
-    autoplay:{delay:5000,},
+    autoplay:{delay: 6000,},
     centeredSlides : true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
 })
+
+let swiperContainer01 = document.querySelector('.content_box_02')
+let swiper01 = new Swiper('.content_box_02', {
+    slidesPerView: '3',
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: false,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    speed: 4000, 
+});
+
+let swiperContainer02 = document.querySelector('.content_box_03')
+let swiper02 = new Swiper('.content_box_03', {
+    slidesPerView: '3',
+    spaceBetween: 10,
+    loop: true,
+    centeredSlides: false,
+    autoplay: {
+        delay: 0,
+        disableOnInteraction: false,
+    },
+    speed: 4000, 
+});
